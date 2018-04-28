07f3ff1ac13af5b5c7b6db094a8fbcaf0fb0860e
teempe <tomasz.pe@zoho.eu>
2018-04-28 17:02:22 +0200

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,12 @@ public class Main {
         Item thirdItem = new Item("blocks", 11.1);
         Item fourItem = new Item("bike", 1001);
 
+        // item added
+
+        Item fourthItem = new Item("item4", 15.5);
+        Item fifthItem = new Item("item5", 20.0);
+        Item sixthItem = new Item("item5", 20.0);
+
         basket.add(firstItem, 5);
         basket.add(secondItem, 2);
         basket.add(thirdItem);
