d6d6ad6e5c2d9597d001cd05d9f949a10ffcff0e
LukPrasek <lukasz.prasek@gmail.com>
2018-04-28 16:36:02 +0200

    Next commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,19 @@ public class Main {
         Item firstItem = new Item("toy", 211.1);
         Item secondItem = new Item("blocks", 11.1);
         Item thirdItem = new Item("blocks", 11.1);
+        Item fourItem = new Item("bike", 1001);
 
         basket.add(firstItem, 5);
         basket.add(secondItem, 2);
         basket.add(thirdItem);
+        basket.add(fourItem);
         System.out.println("Before removal:" + "\n" + basket.toString());
         basket.remove(firstItem, 2);
         basket.remove(thirdItem);
         System.out.println("After removal" + "\n" + basket.toString());
+
+
+
+
     }
 }
